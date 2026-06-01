86fcb1ffb5bb494ddce87f8b7c259f26d692c909
zehongdev <zephyr339@outlook.com>
2026-06-02 01:14:31 +0800

    dist commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  legacy({
+    targets: ["ie>=11"]
+  })
+  ],
+  base: './',
+  build: {
+    target: ['es2015', 'chrome63']
+  }
 })
